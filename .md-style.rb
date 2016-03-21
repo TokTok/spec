f0d2e9054e4f8f308c31f66e00c463c489bec621
iphydf <iphydf@users.noreply.github.com>
2016-03-21 11:48:45 +0000

    Add k-buckets specification.

--- .md-style.rb
+++ .md-style.rb
@@ -7,3 +7,5 @@ rule 'MD029', :style => 'ordered'
 # Pandoc generates lists with the following configured spacing after the list
 # marker.
 rule 'MD030', :ul_single => 3, :ol_single => 2, :ul_multi => 3, :ol_multi => 2
+# We use a % style header for rustdoc.
+exclude_rule 'MD041'
